e5d0a89639c32ea09ce05bc91c78b22b23f619cd
rleandroruiz <leo1998.04.24@hotmail.com>
2017-11-01 11:44:52 -0300

    Avances en interSort

--- InterSort.cpp
+++
@@ -1,6 +0,0 @@
-#include "InterSort.h"
-
-template <class T>
-void interSort(T *vect, unsigned long tamanio){
-
-}
--- InterSort.h
+++ InterSort.h
@@ -3,6 +3,36 @@
 
 
 template <class T>
-void interSort(T *vect, unsigned long tamanio);
+void interSort(T *vect, unsigned long tamanio){
+
+    int k = 2;
+    int j, n1, n2, b1, b2;
+    while(k < tamanio) {
+        for (int i = 0; i < tamanio; i += k) {
+
+            j = i;
+            n1 = i;
+            b1 = i;
+            n2 = i + (k / 2);
+            b2 = i + (k / 2);
+
+            while(n1 < (b1 + k)  || n2 < (b2 + k)) {
+
+                if (vect[n1] < vect[n2]) {
+                    vect[j] = vect[n1];
+                    n1++;
+                    j++;
+                }
+                else {
+                    vect[j] = vect[n2];
+                    n2++;
+                    j++;
+                }
+            }
+        }
+        k *= 2;
+    }
+
+}
 
 #endif //IINTERSORT_H
